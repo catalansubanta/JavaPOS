76b7ef95a35283f7a3c2c3ab160272dddd902dcb
catalansubanta <67074326+catalansubanta@users.noreply.github.com>
2025-04-17 17:29:11 +0545

    Add files via upload
    added init and seed files

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Cart` (`Cart_ID`, `User_ID`, `Item_ID`, `Quantity`, `Created_At`) VALUES
+INSERT INTO `Items` (`Item_ID`, `Item_Name`, `Description`, `Price`, `Category`, `Stock`, `Unit`) VALUES
+INSERT INTO `OrderItems` (`OrderItem_ID`, `Order_ID`, `Item_ID`, `Quantity`, `Price`, `Subtotal`) VALUES
+INSERT INTO `Payment` (`Payment_ID`, `Order_ID`, `Paid_Amount`, `Payment_Time`, `Status`) VALUES
+INSERT INTO `Table_No` (`Table_ID`, `Table_Number`, `Status`) VALUES
+INSERT INTO `Users` (`User_ID`, `Role`, `Username`, `Password`, `Full_Name`, `Email`, `Phone`) VALUES
